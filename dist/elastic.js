b73459ef308ee204f875b66ded0aa7f3d6fb3323
Vitaly Perminov <vitaly.perminov@wrenliving.com>
2016-04-06 10:49:00 +0300

    Add reverse nested aggregation

--- dist/elastic.js
+++ dist/elastic.js
@@ -1,6 +1,6 @@
-/*! elastic.js - v1.2.0 - 2014-10-13
+/*! elastic.js - v1.2.1 - 2016-04-06
  * https://github.com/fullscale/elastic.js
- * Copyright (c) 2014 FullScale Labs, LLC; Licensed MIT */
+ * Copyright (c) 2016 FullScale Labs, LLC; Licensed MIT */
 
 /**
  @namespace
@@ -4985,6 +4985,34 @@
       }
 
     });
+  };
+
+  /**
+   @class
+       <p>A special single bucket aggregation that enables aggregating on parent docs from nested documents.</p>
+
+   @name ejs.ReverseNestedAggregation
+   @ejs aggregation
+   @borrows ejs.AggregationMixin.aggregation as aggregation
+   @borrows ejs.AggregationMixin.agg as agg
+   @borrows ejs.AggregationMixin._type as _type
+   @borrows ejs.AggregationMixin.toJSON as toJSON
+
+   @desc
+   <p>A special single bucket aggregation that enables aggregating on parent docs from nested documents.</p>
+
+   @param {String} name The name which be used to refer to this aggregation.
+
+   */
+  ejs.ReverseNestedAggregation = function (name) {
+
+    var
+      _common = ejs.AggregationMixin(name),
+      agg = _common.toJSON();
+
+    agg[name].reverse_nested = { };
+
+    return extend(_common, { });
   };
 
   /**
